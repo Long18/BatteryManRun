07bdf621657340fd51c248a082904cbf363ac940
WiIIiam <us.thanhlong18@gmail.com>
2022-02-15 22:19:03 +0700

    fix bug and upload file

---
+++ Source/BatteryManRun/BatteryManRunGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BatteryManRunGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABatteryManRunGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABatteryManRunGameMode();
+};
+
+
+
